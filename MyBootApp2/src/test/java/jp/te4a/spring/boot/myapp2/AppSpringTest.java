2ec6be0095bfbc81a1533f29a3bb737c166c5aa4
te180636 <84059840+te180636@users.noreply.github.com>
2021-09-30 19:30:31 +0900

    Update AppSpringTest.java

--- MyBootApp2/src/test/java/jp/te4a/spring/boot/myapp2/AppSpringTest.java
+++ MyBootApp2/src/test/java/jp/te4a/spring/boot/myapp2/AppSpringTest.java
@@ -21,18 +21,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-
-//SpringBootの起動クラスを指定
 @ContextConfiguration(classes = App.class)
-//クラス内の全メソッドにおいて、実行前にDIコンテナの中身を破棄する設定
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-//テストランナー：各テストケース（テストメソッド）を制御する：DIする場合は必須かも
 @ExtendWith(SpringExtension.class)
-//MockおよびWACの自動ロードサーブレット環境を自動作成する
 @AutoConfigureMockMvc
-// テスト時に起動するSpringBootプロジェクトの使用ポート番号を設定する。
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//クラス単位でインスタンスを作成
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AppSpringTest {
     @Autowired
@@ -41,12 +34,12 @@ public class AppSpringTest {
     WebApplicationContext wac;
     
     @BeforeAll
-    public void テスト前処理(){
+    public void テスト前(){
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
     @Test
-    public void te_Taroにアクセス() throws Exception{
+    public void アクセス() throws Exception{
         MvcResult result = 
         mockMvc.perform(get("/taro"))
         .andExpect(status().is2xxSuccessful())
